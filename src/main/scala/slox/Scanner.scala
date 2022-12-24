36781d8479ab0f235b9a19abef5c56993274ac99
Collin Arnett <collin@arnett.it>
2022-12-24 18:01:09 -0500

    fix: scanner not consuming second quotation

--- src/main/scala/slox/Scanner.scala
+++ src/main/scala/slox/Scanner.scala
@@ -10,14 +10,14 @@ case class Scanner(val src: String):
       state: ScannerState = ScannerState("", src, 0),
       tokens: Tokens = Tokens(Seq())
   ): Tokens =
-    if (state.source.isEmpty()) then return tokens
+    val source = state.source
+    if (source.isEmpty()) then return tokens
     val (tokenOption: Option[Token], _state: ScannerState) = scanToken(
-      state.source.head,
-      ScannerState(
-        state.source.head.toString(),
-        state.source.tail,
-        state.line
-      )
+      source.head,
+      state
+        .uLex(source.head.toString())
+        .uSrc(source.tail)
+        .uLin(state.line)
     )
     tokenOption match
       case Some(token) => scanTokens(_state, tokens :+ token)
@@ -27,26 +27,27 @@ case class Scanner(val src: String):
       state: ScannerState
   ): (Option[Token], ScannerState) =
     c match
-      case '(' => addSingle(LEFT_PAREN, state)
-      case ')' => addSingle(RIGHT_PAREN, state)
-      case '{' => addSingle(LEFT_BRACE, state)
-      case '}' => addSingle(RIGHT_BRACE, state)
-      case ',' => addSingle(COMMA, state)
-      case '.' => addSingle(DOT, state)
-      case '-' => addSingle(MINUS, state)
-      case '+' => addSingle(PLUS, state)
-      case ';' => addSingle(SEMICOLON, state)
-      case '*' => addSingle(STAR, state)
-      case '!' => addCouple(BANG, BANG_EQUAL, state)
-      case '=' => addCouple(EQUAL, EQUAL_EQUAL, state)
-      case '<' => addCouple(LESS, LESS_EQUAL, state)
-      case '>' => addCouple(GREATER, GREATER_EQUAL, state)
+      case '(' => addToken(LEFT_PAREN, state)
+      case ')' => addToken(RIGHT_PAREN, state)
+      case '{' => addToken(LEFT_BRACE, state)
+      case '}' => addToken(RIGHT_BRACE, state)
+      case ',' => addToken(COMMA, state)
+      case '.' => addToken(DOT, state)
+      case '-' => addToken(MINUS, state)
+      case '+' => addToken(PLUS, state)
+      case ';' => addToken(SEMICOLON, state)
+      case '*' => addToken(STAR, state)
+      case '!' => addToken(BANG, BANG_EQUAL, state)
+      case '=' => addToken(EQUAL, EQUAL_EQUAL, state)
+      case '<' => addToken(LESS, LESS_EQUAL, state)
+      case '>' => addToken(GREATER, GREATER_EQUAL, state)
       case '/' =>
-        state.source.head match
-          case '/' =>
+        state.source.headOption match
+          case Some('/') =>
             val (head, tail) = state.source.span(_ != '\n')
-            (None, ScannerState("", state.source.tail, state.line))
-          case _ => addSingle(SLASH, state)
+            (None, state.uSrc(state.source.tail))
+          case None => addToken(SLASH, state.uSrc(""))
+          case _    => addToken(SLASH, state)
       case ' ' | '\r' | '\t' => (None, state)
       case '\n' =>
         (None, ScannerState("", state.source, state.line + 1))
@@ -58,19 +59,17 @@ case class Scanner(val src: String):
         else
           (
             Some(Token(STRING, head, head, state.line)),
-            ScannerState(
-              "",
-              state.source.tail,
-              state.line + head.count(_ == '\n')
-            )
+            state
+              .uSrc(tail.tail)
+              .uLin(state.line + head.count(_ == '\n'))
           )
       case _ => (None, state)
-  private def addSingle(
+  private def addToken(
       _type: TokenType,
       state: ScannerState
   ): (Option[Token], ScannerState) =
     (Some(Token(_type, state.lexeme, None, state.line)), state)
-  private def addCouple(
+  private def addToken(
       op1: One,
       op2: Two,
       state: ScannerState
@@ -83,11 +82,15 @@ case class Scanner(val src: String):
           Some(
             Token(op2, state.lexeme + source.head, None, line)
           ),
-          ScannerState("", source.tail, line)
+          state.uSrc(source.tail)
         )
       case _ => (Some(Token(op1, state.lexeme, None, line)), state)
+
 case class ScannerState(
     val lexeme: String,
     val source: String,
     val line: Int
-)
+):
+  def uLex(lexeme: String) = this.copy(lexeme = lexeme)
+  def uSrc(source: String) = this.copy(source = source)
+  def uLin(line: Int) = this.copy(line = line)
